ad06eae8438f4b19a760ddce9da935712201388c
Manan Patel <mananpatel.711@gmail.com>
2021-03-04 19:47:36 +0530

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
     $('.parallax').parallax();
     $('.sidenav').sidenav();
     $('.slider').slider({full_width: true})
+    $('.parallax').parallax();
+    $('.myreviews').carousel({
+        numVisible: 7,
+        shift: 55,
+        padding: 55
+    });
 });
 
 function toggleModal() {
